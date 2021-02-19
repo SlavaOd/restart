e76a64693dfac5bb968a55fd3b19b86c0fdec79d
SlavaOd <sbodryan1992@gmail.com>
2021-02-19 21:20:17 +0200

    add

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ var upsell = document.querySelector('.upsell')
 var main = document.querySelector('.main')
 var back = document.querySelector('.back')
 var fix = document.querySelector('.fix')
-var itog = document.querySelector('.itog')
-var itog2 = document.querySelector('.itog2')
+var out = document.querySelector('.out')
+var out_add = document.querySelector('.out_add')
 var quantity = document.querySelector('.quantity')
 
-function fun1() {
-
+function region() {
     var r = document.getElementsByName('radio');
         for (var i=0;i<r.length; i++) {
         if (r[i].checked && i == 0) {
@@ -26,8 +25,13 @@ function fun1() {
     }
 }
 
+function percentCreate() {
+    percent = upsell.value / resNum
+    bonus = percent.toFixed(2) * resSum
+}
+
 result.onclick = function() {
-    fun1()
+    region()
     x = rozvag.value
     y = video.value
     resSum = (x*30) + (y*50)
@@ -35,27 +39,22 @@ result.onclick = function() {
     reg.innerHTML = 'Разом:' + '<br>' + 'Реєстрацій: ' + resNum + 'шт ' +  '<br>' + ' Бонус: ' + resSum + 'грн '
     if (resNum < pln) {
         plan = pln - resNum
-        itog.style.color = 'red'
-        itog.innerHTML = 'Ви зробили меньше ' + pln + ' реєстрацій. Щоб отримати додатковий бонус зареєструйте ще ' + plan + ' СП'
-        itog2.innerHTML = ''
+        out.style.color = 'red'
+        out.innerHTML = 'Ви зробили меньше ' + pln + ' реєстрацій. Щоб отримати додатковий бонус зареєструйте ще ' + plan + ' СП'
+        out_add.innerHTML = ''
     } else if (upsell.value < pln) {
         plan = pln - upsell.value
-        itog.style.color = 'red'
-        itog.innerHTML = 'Ви зробили меньше ' + pln + ' апсельних операцій. Щоб отримати додатковий бонус зробіть ще ' + plan
-        itog2.innerHTML = ''
-
-    }
-    else if ( upsell.value >= resNum ) {
+        out.style.color = 'red'
+        out.innerHTML = 'Ви зробили меньше ' + pln + ' апсельних операцій. Щоб отримати додатковий бонус зробіть ще ' + plan
+        out_add.innerHTML = ''
+    } else if ( upsell.value >= resNum ) {
         restart = resSum*2
-       itog.innerHTML = 'Додатковий бонус : ' +  resSum + 'грн'
-       itog2.innerHTML = 'Загальний бонус : ' +  restart  + 'грн'
+        out.innerHTML = 'Додатковий бонус : ' +  resSum + 'грн'
+        out_add.innerHTML = 'Загальний бонус : ' +  restart  + 'грн'
     } else {
-        percent = upsell.value / resNum
-        console.log(percent.toFixed(2) * resSum)
-        bonus = percent.toFixed(2) * resSum
-        // restart = resSum +  resSum / 2
-        itog.innerHTML = 'Додатковий бонус: ' +  Math.floor(percent.toFixed(2) * resSum) + 'грн'
-        itog2.innerHTML = 'Загальний бонус : ' + Math.floor(resSum + bonus) + 'грн'
+        percentCreate()
+        out.innerHTML = 'Додатковий бонус: ' +  Math.floor(percent.toFixed(2) * resSum) + 'грн'
+        out_add.innerHTML = 'Загальний бонус : ' + Math.floor(resSum + bonus) + 'грн'
         
     } 
     
